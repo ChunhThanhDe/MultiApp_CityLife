2ae738feb7123851b682616c43db6c06b72ca8bb
tuan.dn <dongoctuan2002sp@gmail.com>
2025-05-15 23:26:55 +0700

    refactor: integrate loading overlay in sign-in process and remove unused animated scale button and app overlay widgets

--- lib/presentation/modules/authentication/sign_in/sign_in_controller.dart
+++ lib/presentation/modules/authentication/sign_in/sign_in_controller.dart
@@ -4,6 +4,7 @@ import 'package:sixam_mart_user/base/base_controller.dart';
 import 'package:sixam_mart_user/domain/models/page_param/sign_in_page_param.dart';
 import 'package:sixam_mart_user/domain/models/request/login_request.dart';
 import 'package:sixam_mart_user/domain/repositories/auth_repository.dart';
+import 'package:sixam_mart_user/presentation/shared/app_overlay.dart';
 
 class SignInController extends BaseController {
   final SignInPageParam? pageParam;
@@ -13,7 +14,7 @@ class SignInController extends BaseController {
   final AuthRepository _authRepository = Get.find<AuthRepository>();
 
   Future<void> login() async {
-    final result = await _authRepository.login(LoginRequest(email: 'test@test.com', password: '123456'));
+    final ApiResult result = await showLoadingOverlay(api: _authRepository.login(LoginRequest(email: 'test@test.com', password: '123456')));
     switch (result) {
       case Success(:final data):
         print(data);
