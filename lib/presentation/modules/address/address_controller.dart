aada2a30ef9add817db5ed6450151be555c55e7a
tuan.dn <dongoctuan2002sp@gmail.com>
2025-05-16 21:12:35 +0700

    refactor: enhance sign-up screen by extracting phone number input and validation logic into separate components for better organization and maintainability

---
+++ lib/presentation/modules/address/address_controller.dart
@@ -0,0 +1,3 @@
+import 'package:sixam_mart_user/base/base_controller.dart';
+
+class AddressController extends BaseController {}
---
+++ lib/presentation/modules/address/address_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sixam_mart_user/base/base_screen.dart';
+
+import 'address_controller.dart';
+
+class AddressScreen extends BaseScreen<AddressController> {
+  const AddressScreen({super.key});
+
+
+  @override
+  Widget buildScreen(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
