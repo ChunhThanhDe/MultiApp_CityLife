f2e176d698d65d61e01dccf53b34148f2c3f017d
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2025-05-19 16:04:06 +0700

    refactor: fix error warning in input text -> simplify PasswordInput widget by removing unnecessary container and streamlining the TextFormField layout

--- lib/presentation/modules/account/components/password_input.dart
+++ lib/presentation/modules/account/components/password_input.dart
@@ -1,4 +1,3 @@
-// Password input field widget
 import 'package:flutter/material.dart';
 
 class PasswordInput extends StatelessWidget {
@@ -24,36 +23,40 @@ class PasswordInput extends StatelessWidget {
       children: [
         Text(label, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF161A1D))),
         const SizedBox(height: 8),
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFFE8EBEE)),
-            borderRadius: BorderRadius.circular(6),
-            color: Colors.white,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: controller,
-                  obscureText: isObscure,
-                  validator: validator,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Enter $label".replaceFirst("password", "password"),
-                    hintStyle: TextStyle(color: Color(0xFF798A9A)),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                  ),
-                ),
+        TextFormField(
+          controller: controller,
+          obscureText: isObscure,
+          validator: validator,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          decoration: InputDecoration(
+            hintText: "Enter $label".replaceFirst("password", "password"),
+            hintStyle: TextStyle(color: Color(0xFF798A9A)),
+            contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            errorStyle: TextStyle(color: Colors.red, fontSize: 12),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              borderSide: BorderSide(color: Color(0xFFE8EBEE)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              borderSide: BorderSide(color: Color(0xFF5856D7)),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(
+                isObscure ? Icons.visibility_off : Icons.visibility,
+                color: Color(0xFFB0BAC4),
+                size: 24,
               ),
-              IconButton(
-                icon: Icon(
-                  isObscure ? Icons.visibility_off : Icons.visibility,
-                  color: Color(0xFFB0BAC4),
-                  size: 24,
-                ),
-                onPressed: onToggle,
-              ),
-            ],
+              onPressed: onToggle,
+            ),
           ),
         ),
       ],
